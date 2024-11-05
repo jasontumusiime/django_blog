ee53511539d40e2d5a8c9f0a440e4ea4025ae742
Jason Tumusiime <jason.2mcme@gmail.com>
2024-11-05 21:03:56 +0300

    Add share post by email feature

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
@@ -7,51 +8,58 @@ from .forms import EmailPostForm
 from .models import Post
 
 
-
 def post_share(request, post_id):
-    post = get_object_or_404(
-        Post, 
-        id=post_id,
-        status=Post.Status.PUBLISHED
-    )
-
-    if request.method == 'POST':
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == "POST":
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            # TODO: Send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you read this post {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url} \n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+            send_mail(subject, message, None, [cd["to"]])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
 
 
 class PostListView(ListView):
     queryset = Post.published.all()
-    context_object_name = 'posts'
+    context_object_name = "posts"
     paginate_by = 1
-    template_name = 'blog/post/list.html'
+    template_name = "blog/post/list.html"
 
 
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 2)
-    page_number = request.GET.get('page', 1)
+    page_number = request.GET.get("page", 1)
     try:
         posts = paginator.page(page_number)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     except PageNotAnInteger:
         posts = paginator.page(1)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, "blog/post/list.html", {"posts": posts})
 
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
-        Post, 
+        Post,
         status=Post.Status.PUBLISHED,
         publish__year=year,
         publish__month=month,
         publish__day=day,
-        slug=post
+        slug=post,
     )
-    return render(request, 'blog/post/detail.html', {'post': post})
+    return render(request, "blog/post/detail.html", {"post": post})
