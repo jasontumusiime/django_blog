09cc194a1b413fff2880bd71ff5077dc97da621c
Jason Tumusiime <jason.2mcme@gmail.com>
2024-11-05 19:11:07 +0300

    Change canonical url for a post to SEO-friendly blog/year/month/day/slug/

--- blog/models.py
+++ blog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
 
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, 
         on_delete=models.CASCADE, 
@@ -43,4 +43,12 @@ class Post(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.id])
+        return reverse(
+            'blog:post_detail', 
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
